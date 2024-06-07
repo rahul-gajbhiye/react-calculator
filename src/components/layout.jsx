a5543136d40dcaeaa262b52096fb6a588664667e
Rahul <rahulgajbhiye1699@gmail.com>
2024-06-07 18:15:08 +0530

    Buttons added

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -41,36 +41,43 @@ const Layout = () => {
       <Row className="d-flex justify-content-center align-items-center vh-100">
         <Col xs={12} md={6} xl={4} onKeyDown={handleKeydown}>
           <div className="text-center border p-4">
-            
-            <div><input type="text" value={display} readOnly className="display w-100 " /></div>
+            <Row>
+            <Col>
+            <input type="text" value={display} readOnly className="display w-100" />
+            </Col>
+            </Row>
             <div>
-              <Row>
-                <Col xs={3} ><Button onClick={backspace}>←</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('+')}>+</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('-')}>-</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('/')}>/</Button></Col>      
-              
-                <Col xs={3} ><Button onClick={() => appendToDisplay('7')}>7</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('8')}>8</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('9')}>9</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('*')}>*</Button></Col>       
-              
-                <Col xs={3} ><Button onClick={() => appendToDisplay('4')}>4</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('5')}>5</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('6')}>6</Button></Col>
-                <Col xs={3} ><Button onClick={() => appendToDisplay('%')}>%</Button></Col>       
-                  
+              <Row >
+                <Col xs={3} ><Button onClick={backspace} className="backspace w-100 m-1">←</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('+')} className="plus w-100 m-1 d-grid">+</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('-')} className="minus w-100 m-1">-</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('/')} className="divide w-100 m-1">/</Button></Col>      
+                <Col xs={3} ><Button onClick={() => appendToDisplay('7')} className="seven w-100 m-1">7</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('8')} className="eight w-100 m-1">8</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('9')} className="nine w-100 m-1">9</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('*')} className="asterik w-100 m-1">*</Button></Col>      
+                <Col xs={3} ><Button onClick={() => appendToDisplay('4')} className="four w-100 m-1">4</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('5')} className="five w-100 m-1">5</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('6')} className="six w-100 m-1">6</Button></Col>
+                <Col xs={3} ><Button onClick={() => appendToDisplay('%')} className="percentage w-100 m-1">%</Button></Col> 
                   <Col xs={9}>
-                    <Col xs={3} ><Button onClick={() => appendToDisplay('1')}>1</Button></Col>
-                    <Col xs={3} ><Button onClick={() => appendToDisplay('2')}>2</Button></Col>
-                    <Col xs={3} ><Button onClick={() => appendToDisplay('3')}>3</Button></Col>
-                    <Col xs={3}><Button onClick={() => appendToDisplay('.')}>.</Button></Col>
-                    <Col xs={3}><Button onClick={() => appendToDisplay('0')} className="zero">0</Button></Col>
-                    <Col xs={3}><Button onClick={clearDisplay}>C</Button></Col>
+                    <Row>
+                    <Col xs={4} ><Button onClick={() => appendToDisplay('1')} className="one w-100 m-1">1</Button></Col>
+                    <Col xs={4} ><Button onClick={() => appendToDisplay('2')} className="two w-100 m-1">2</Button></Col>
+                    <Col xs={4} ><Button onClick={() => appendToDisplay('3')} className="three w-100 m-1">3</Button></Col>
+                    <Col xs={4}><Button onClick={() => appendToDisplay('.')} className="dot w-100 m-1">.</Button></Col>
+                    <Col xs={4}><Button onClick={() => appendToDisplay('0')} className="zero w-100 m-1">0</Button></Col>
+                    <Col xs={4}><Button onClick={clearDisplay} className="clear w-100 m-1">C</Button></Col>
+                    </Row>
                   </Col>
                   <Col xs={3}>
-                    <Col><Button onClick={calculate} className="equal">=</Button></Col>
+                    <Row>
+                    <Col><Button onClick={calculate} className="equal w-100 m-1">=</Button></Col>
+                    
+                    </Row>
                   </Col>
+                  
+              
                       
               </Row>
             </div>
